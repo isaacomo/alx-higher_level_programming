bba696961c20927db3b687c2ad379542c17f78b8
isaacomo <aas.isaac26@gmail.com>
2022-12-07 10:27:07 -0800

    fix

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string)
+roman_to_int = __import__('12-roman_to_int').roman_to_int
+roman_number = "X"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = "VII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
