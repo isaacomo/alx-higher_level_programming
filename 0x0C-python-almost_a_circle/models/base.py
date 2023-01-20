70da2edd05f74dcc7ba5db9074bac75e296d3fa4
isaacomo <aas.isaac26@gmail.com>
2023-01-20 01:42:24 -0800

    0x0C. Python - Almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Base:#python3 -c 'print(__import__("my_module").MyClass.__doc__)'#
+    def __init__(self, id=None):
+        self.id = None
+
+        __nb_objects = 0
+            if (id!=None):
+                self.id = n
+            __nb_objects += 1
+                self.id = b
+
+
