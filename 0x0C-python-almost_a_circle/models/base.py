15aca3af8b8afde8f8367c4e5c078fada41d64bb
isaacomo <aas.isaac26@gmail.com>
2023-01-20 11:45:06 -0800

    correction

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-""" Class Base will be the 'base' of all other classes """
 
 
 class Base:
-    """ This class will be the 'base' of all other classes """
-
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ init method """
         if id is not None:
             self.id = id
         else:
