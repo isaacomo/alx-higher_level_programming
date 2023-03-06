c208c0a431788a9915e018bcaa189277b1747ef2
omorowange isaac <aas.isaac26@gmail.com>
2023-03-06 14:15:05 -0800

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- creates a database and a table if not already existing
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states
+(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
