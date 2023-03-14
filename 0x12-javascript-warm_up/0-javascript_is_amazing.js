d0c6eff66d224b34a59b9d8fb58fc412b45ba3d2
omorowange isaac <aas.isaac26@gmail.com>
2023-03-13 23:07:21 -0700

    my fix

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/*
+/**
  * Print string “Javascript is amazing”
  */
 
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
-// Prints a message depending of the number of arguments passed
+/* 
+ * Prints a message depending of the number of arguments passed
+ */
 
-if (process.argv.length === 2) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
+if (process.argv.length === 2) 
+{
+	console.log('No argument');
+} 
+else if (process.argv.length === 3) 
+{
+	console.log('Argument found');
+} 
+else 
+{
+	console.log('Arguments found');
 }
